d81ac8c751724c798d895ee312b67f65d1de2e0b
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-02-28 11:39:13 +0500

    CoreImage
    Learning how to convert from different types of Images

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -4,28 +4,37 @@
 //
 //  Created by Насрулло Исмоилжонов on 27/02/24.
 //
-
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     var body: some View {
-        Button("Hello, World!") {
-                    showingConfirmation = true
-                }
-                .frame(width: 300, height: 300)
-                .background(backgroundColor)
-                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                    Button("Red") { backgroundColor = .red }
-                    Button("Green") { backgroundColor = .green }
-                    Button("Blue") { backgroundColor = .blue }
-                    Button("Cancel", role: .cancel) { }
-                } message: {
-                    Text("Select a new color")
-                }
+        VStack{
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage(){
+        let inputImage = UIImage(resource: .de4EC4630DD94B9AA12AAD4E927E443B1105C)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
+        
+        guard let outputImage = currentFilter.outputImage else {return}
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
     }
 }
 
